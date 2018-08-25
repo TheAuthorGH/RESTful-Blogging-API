bc8a32c8527620817398a5576c10ade0637a1f5a
Luis Lau <theauthorgm@gmail.com>
2018-08-24 19:58:18 -0500

    improved tests

--- test/test-blogpost.js
+++ test/test-blogpost.js
@@ -11,6 +11,7 @@ const modelPost = {
 	publishDate: '8/24/2018'
 }
 
+
 describe('BlogPost API', function() {
 	before(startServer);
 	after(stopServer);
@@ -21,6 +22,10 @@ describe('BlogPost API', function() {
 			.get('/blog-posts')
 			.then(function(res) {
 				expect(res).to.have.status(200);
+				expect(res).to.be.json;
+				expect(res.body).to.be.a('array');
+				for(let i of res.body)
+					expect(i).to.include.keys(['id', 'title', 'content', 'author', 'publishDate']);
 			});
 	});
 
@@ -31,6 +36,9 @@ describe('BlogPost API', function() {
 			.send(Object.create(modelPost))
 			.then(function(res) {
 				expect(res).to.have.status(201);
+				expect(res).to.be.json;
+				expect(res.body).to.be.a('object');
+				expect(res.body).to.include.keys(['id', 'title', 'content', 'author', 'publishDate']);
 			})
 	});
 
