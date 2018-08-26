c7e48d9b4ac0fbcc340bcdd0292dd85b2f7b2a90
Luis Lau <theauthorgm@gmail.com>
2018-08-26 14:25:02 -0500

    added a server config file

---
+++ config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports.PORT = process.env.PORT || 8080;
+module.exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogapi-db';
+module.exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blogapi-db-testing';
